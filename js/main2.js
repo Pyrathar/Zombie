44a3cddf642fd2c808e42a08b23c4b0212e6a9d2
Pyrathar <blacklorathal@gmail.com>
2017-09-02 19:58:24 +0000

    Memory Loss

--- js/main2.js
+++ js/main2.js
@@ -7,7 +7,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 var riddle = null;
 var riddleanswer= "";
 var combo = 0;
-var zombie;
+var sprites;
 
 function preload() {
 
@@ -16,8 +16,8 @@ function preload() {
     this.load.spritesheet('ninja', 'assets/ninja/ninja.png', 232, 439);
     this.load.spritesheet('zombie', 'assets/zombie/zombie.png', 430, 519);
 
-}
 
+}
 
 function create() {
     this.background = this.game.add.sprite(0, 0, 'background');
@@ -25,40 +25,43 @@ function create() {
     this.ninja.scale.setTo(0.30);
     this.ninja.animations.add('left', [0, 1, 2, 3], 10, true);
     this.ninja.animations.play('left');
-    zombie = game.add.group();
-    createzombie();
+    game.physics.enable([this.ninja], Phaser.Physics.ARCADE);
 
+    sprites = game.add.group();
+    game.time.events.loop(50, createzombie, this);
 
     maingame();
+    createzombie();
+
+    input();
 
 
 
-    //game.physics.enable([this.zombie, this.ninja], Phaser.Physics.ARCADE);
 
 
 }
 
 function createzombie(){
-
-       zombie.create(game.world.game.rnd.integerInRange(100, 200), game.world.game.rnd.integerInRange(100, 200), 'zombie');
-       zombie.scale.setTo(0.30)
-       zombie.add('walk')
-
-         if (zombie.x < -zombie.width) {
-            zombie.x = game.world.width;
-         }
+  var zombie = sprites.create(0,game.world.randomY,'zombie');
+  zombie.scale.setTo(-0.30, 0.30);
+  zombie.animations.add('left', [0, 1, 2, 3], 10, true);
+  zombie.animations.play('left');
 
 
 }
 
+
 function update() {
 
 
+   sprites.setAll('x', 10, true, true, 1);
+
+    sprites.forEach(createzombie, this, true);
 
 
 
+    game.physics.arcade.collide(zombie, this.ninja, collisionHandler, null, this);
 
-    game.physics.arcade.collide(this.zombie, this.ninja, collisionHandler, null, this);
 
 
     if (this.shuriken!=null){
@@ -72,60 +75,54 @@ function collisionHandler(obj1, obj2) {
     if (obj1==this.shuriken&&obj2==this.zombie){
       console.log("dead")
 
-      obj2.destroy();
+      zombie.destroy();
     }
     game.stage.backgroundColor = '#992d2d';
 
 }
 
-
 function input() {
 
-      ////////////////////////////////////////////////////////////////Key Input////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////Key Input////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-      $(function() {
-          var input = [];
-          $(window).keypress(function(e) {
-              var key = e.which;
+    $(function() {
+        var input = [];
+        $(window).keypress(function(e) {
+            var key = e.which;
 
-              //List of key-codes->https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+            //List of key-codes->https://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
 
-              switch (key) {
-                  ////////////////Enter Key Behavior//////////////////
-                  case 13:
-                      var clear = String.fromCharCode.apply(null, input);
-                      if (clear==riddleanswer){
-                      console.log("yup");
-                      this.shuriken = this.game.add.sprite(100, 160, 'shuriken');
-                      game.physics.enable([this.shuriken], Phaser.Physics.ARCADE);
-                      this.shuriken.body.velocity.x= +200;
-                      collisionHandler(this.shuriken,this.zombie)
-                      maingame();
-                      }
-                      input = []
-                      break;
-                      ////////////////Backspace key Behavior//////////////////
-                  case 8:
+            switch (key) {
+                ////////////////Enter Key Behavior//////////////////
+                case 13:
+                    var clear = String.fromCharCode.apply(null, input);
+                    if (clear==riddleanswer){
+                    console.log("yup");
+                    this.shuriken = this.game.add.sprite(100, 160, 'shuriken');
+                    game.physics.enable([this.shuriken], Phaser.Physics.ARCADE);
+                    this.shuriken.body.velocity.x= +200;
+                    collisionHandler(this.shuriken,this.zombie)
+                    maingame();
+                    }
+                    input = []
+                    break;
+                    ////////////////Backspace key Behavior//////////////////
+                case 8:
 
-                      console.log("something else pressed space pressed");
+                    console.log("something else pressed space pressed");
 
-                      break;
-                      ////////////////Rest of keys Behavior//////////////////
-                  default:
-                      input.push(key)
-                      var clear = String.fromCharCode.apply(null, input);
-                      //console.log(String.fromCharCode.apply(null, input));
-                      //console.log(clear);
-              }
+                    break;
+                    ////////////////Rest of keys Behavior//////////////////
+                default:
+                    input.push(key)
+                    var clear = String.fromCharCode.apply(null, input);
+                    //console.log(String.fromCharCode.apply(null, input));
+                    //console.log(clear);
+            }
 
-          });
-      });
-      ////////////////////////////////////////////////////////////////Key Input END////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-}
-
-function shootquestion(question) {
-
+        });
+    });
+    ////////////////////////////////////////////////////////////////Key Input END////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
 
 function maingame(){
@@ -155,20 +152,4 @@ function maingame(){
 
 
 }
-
-function createzombie(){
-
-       zombie.create(game.world.game.rnd.integerInRange(100, 200), game.world.game.rnd.integerInRange(100, 200), 'zombie');
-       zombie.scale.setTo(0.30)
-       zombie.animations.add('walk');
-       zombie.animations.play('walk',[0, 1, 2, 3], 10, true);
-
-         if (zombie.x < -zombie.width) {
-            zombie.x = game.world.width;
-         }
-
-
-}
-
-
 }
